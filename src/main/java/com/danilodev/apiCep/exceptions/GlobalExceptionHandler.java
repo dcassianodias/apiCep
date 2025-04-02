d8df63c59e5e310acf366c5762609d97e9efdfdf
Danilo Dias <dcassianodias@gmail.com>
2025-04-02 01:08:13 -0300

    fix: correção de anotação.

--- src/main/java/com/danilodev/apiCep/exceptions/GlobalExceptionHandler.java
+++ src/main/java/com/danilodev/apiCep/exceptions/GlobalExceptionHandler.java
@@ -5,12 +5,12 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseStatus;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
 
 import java.util.HashMap;
 import java.util.Map;
 
-@RestController
+@RestControllerAdvice
 public class GlobalExceptionHandler {
 
     @ExceptionHandler(IllegalArgumentException.class)
