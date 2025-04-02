b08ab7d05ced365411ded23e71822520b7620c70
Danilo Dias <dcassianodias@gmail.com>
2025-04-02 14:45:26 -0300

    refactor: ajuste da Service para padrões do SOLID

--- src/main/java/com/danilodev/apiCep/domain/service/CepService.java
+++ src/main/java/com/danilodev/apiCep/domain/service/CepService.java
@@ -2,7 +2,10 @@ package com.danilodev.apiCep.domain.service;
 
 import com.danilodev.apiCep.domain.dto.CepResponseDTO;
 import com.danilodev.apiCep.domain.model.CepLog;
+import com.danilodev.apiCep.domain.model.CepLogStorage;
 import com.danilodev.apiCep.domain.repository.CepLogRepository;
+import com.danilodev.apiCep.domain.validators.CepValidator;
+import com.danilodev.apiCep.integration.CepLookup;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
 
@@ -12,30 +15,24 @@ import java.util.regex.Pattern;
 @Service
 public class CepService {
 
+    private final CepLookup cepClient;
+    private final CepValidator cepValidator;
+    private final CepLogStorage cepLogStorage;
 
-    private final CepLogRepository cepLogRepository;
-    private final RestTemplate resTemplate = new RestTemplate();
-    private final String MOCK_API_URL = "http://localhost:8081/cep/";
-
-    public CepService(CepLogRepository cepLogRepository) {
-        this.cepLogRepository = cepLogRepository;
+    public CepService(CepLookup cepClient, CepValidator cepValidator, CepLogStorage cepLogStorage) {
+        this.cepClient = cepClient;
+        this.cepValidator = cepValidator;
+        this.cepLogStorage = cepLogStorage;
     }
 
     public CepResponseDTO buscarCep(String cep) {
-        if (!isValidCep(cep)) {
-            throw new IllegalArgumentException("CEP Inválido");
-        }
+        cepValidator.validate(cep);
+        String response = cepClient.buscarCep(cep);
 
-        String response = resTemplate.getForObject(MOCK_API_URL + cep, String.class);
         LocalDateTime timestamp = LocalDateTime.now();
-
         CepLog log = new CepLog(null, cep, response, timestamp);
-        cepLogRepository.save(log);
+        cepLogStorage.salvar(log);
 
         return new CepResponseDTO(cep, response, timestamp);
-    }
-
-    private boolean isValidCep(String cep){
-        return Pattern.matches("\\d{8}", cep);
     }
 }
